68756d86e1ddad451a10b3be7861b95f6946ddb0
POOVARASAN S <sivanperumal30772@gmail.com>
2023-11-02 11:39:12 +0530

    final

--- routes/category/get.go
+++ routes/category/get.go
@@ -401,7 +401,6 @@ type MyEvents struct {
 	User2_Name   string `json:"user_2_name"`
 	User3        string `json:"user_3"`
 	User3_Name   string `json:"user_3_name"`
-
 	EIncharge    string `json:"eincharge"`
 	CIncharge    string `json:"cincharge"`
 	EventName    string `json:"event_name"`
@@ -422,7 +421,7 @@ func GetMyEvents(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	row := config.Database.QueryRow(
-		"SELECT er.team_name, er.user_1, er.user_2, er.user_3, er.event_category_id, mu1.name AS user_1_name, mu2.name AS user_2_name, mu3.name AS user_3_name, muu.name AS event_incharge, me.event_name, me.description AS edescription, mc.category_name AS cname,mc.incharge AS cincharge, mc.description AS cdescription,muc.name AS cincharge, me.event_date FROM `event_register` er INNER JOIN `event_categories` ec ON ec.`id` = er.`event_category_id` INNER JOIN m_category mc ON mc.id=er.event_category_id INNER JOIN m_events me ON me.id = ec.`event_id` INNER JOIN m_users muu ON muu.id=me.incharge INNER JOIN `m_users` mu1 ON mu1.id = er.`user_1` INNER JOIN m_users mu2 ON mu2.id = er.`user_2` INNER JOIN m_users mu3 ON mu3.id = er.`user_3` INNER JOIN m_users muc ON muc.id=mc.incharge WHERE (er.user_1 = ? OR er.user_2 = ? OR er.user_3 = ?)",
+		"SELECT er.team_name, er.user_1, er.user_2, er.user_3, er.event_category_id, mu1.name AS user_1_name, mu2.name AS user_2_name, mu3.name AS user_3_name, muu.name AS event_incharge, me.event_name, me.description AS edescription, mc.category_name AS cname, mc.description AS cdescription,muc.name AS cincharge, me.event_date FROM `event_register` er INNER JOIN `event_categories` ec ON ec.`id` = er.`event_category_id` INNER JOIN m_category mc ON mc.id=er.event_category_id INNER JOIN m_events me ON me.id = ec.`event_id` INNER JOIN m_users muu ON muu.id=me.incharge INNER JOIN `m_users` mu1 ON mu1.id = er.`user_1` INNER JOIN m_users mu2 ON mu2.id = er.`user_2` INNER JOIN m_users mu3 ON mu3.id = er.`user_3` INNER JOIN m_users muc ON muc.id=mc.incharge WHERE (er.user_1 = ? OR er.user_2 =? OR er.user_3 = ?)",
 		requestData.UserID, requestData.UserID, requestData.UserID)
 	
 	var events MyEvents
