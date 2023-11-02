ac39a9b39decf69e596acb372d99ee9e5615952a
POOVARASAN S <sivanperumal30772@gmail.com>
2023-11-02 12:36:23 +0530

    final

--- routes/category/get.go
+++ routes/category/get.go
@@ -23,12 +23,12 @@ type Category struct {
 }
 
 type CategoryC struct {
-	Id           int    `json:"id"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	InchargeName string `json:"incharge"`
-	MaxTeam      int    `json:"max_team"`
-	Registerstatus int `json:"registration"`
+	Id             int    `json:"id"`
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	InchargeName   string `json:"incharge"`
+	MaxTeam        int    `json:"max_team"`
+	Registerstatus int    `json:"registration"`
 }
 
 type Input struct {
@@ -96,7 +96,7 @@ func GetDetail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = config.Database.QueryRow("SELECT mc.id,mc.category_name,mc.description,mu.name,mc.max_team,mc.registration FROM event_categories ec INNER JOIN m_category mc ON mc.id = ec.category_id INNER JOIN m_events mee ON mee.id = ec.event_id INNER JOIN m_users mu ON mu.id = mc.incharge WHERE ec.status = '1' AND ec.category_id = ?", input.Id).Scan(&categories.Id, &categories.Name, &categories.Description, &categories.InchargeName, &categories.MaxTeam,&categories.Registerstatus)
+	err = config.Database.QueryRow("SELECT mc.id,mc.category_name,mc.description,mu.name,mc.max_team,mc.registration FROM event_categories ec INNER JOIN m_category mc ON mc.id = ec.category_id INNER JOIN m_events mee ON mee.id = ec.event_id INNER JOIN m_users mu ON mu.id = mc.incharge WHERE ec.status = '1' AND ec.category_id = ?", input.Id).Scan(&categories.Id, &categories.Name, &categories.Description, &categories.InchargeName, &categories.MaxTeam, &categories.Registerstatus)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -403,21 +403,21 @@ func CheckTeam(w http.ResponseWriter, r *http.Request) {
 
 // to get my events data
 type MyEvents struct {
-	TeamName     string `json:"team_name"`
-	User1        string `json:"user_1"`
-	User1_Name   string `json:"user_1_name"`
-	User2        string `json:"user_2"`
-	User2_Name   string `json:"user_2_name"`
-	User3        string `json:"user_3"`
-	User3_Name   string `json:"user_3_name"`
-	EIncharge    string `json:"eincharge"`
-	CIncharge    string `json:"cincharge"`
-	EventName    string `json:"event_name"`
-	Edesciption  string `json:"edescription"`
-	EventDate    string `json:"event_date"`
-	CategoryName string `json:"cname"`
-	CDescription string `json:"cdescription"`
-	Category_id  int    `json:"event_category_id"`
+	TeamName     string  `json:"team_name"`
+	User1        string  `json:"user_1"`
+	User1_Name   string  `json:"user_1_name"`
+	User2        *string `json:"user_2"`
+	User2_Name   *string `json:"user_2_name"`
+	User3        *string `json:"user_3"`
+	User3_Name   *string `json:"user_3_name"`
+	EIncharge    string  `json:"eincharge"`
+	CIncharge    string  `json:"cincharge"`
+	EventName    string  `json:"event_name"`
+	Edesciption  string  `json:"edescription"`
+	EventDate    string  `json:"event_date"`
+	CategoryName string  `json:"cname"`
+	CDescription string  `json:"cdescription"`
+	Category_id  int     `json:"event_category_id"`
 }
 
 func GetMyEvents(w http.ResponseWriter, r *http.Request) {
@@ -432,15 +432,13 @@ func GetMyEvents(w http.ResponseWriter, r *http.Request) {
 	row := config.Database.QueryRow(
 		"SELECT er.team_name, er.user_1, mu1.name AS user_1_name, er.user_2,mu2.name AS user_2_name, er.user_3,mu3.name AS user_3_name, muu.name AS event_incharge,muc.name AS cincharge, me.event_name, me.description AS edescription,er.event_category_id, mc.category_name AS cname, mc.description AS cdescription, me.event_date FROM `event_register` er INNER JOIN `event_categories` ec ON ec.`id` = er.`event_category_id` INNER JOIN m_category mc ON mc.id=er.event_category_id INNER JOIN m_events me ON me.id = ec.`event_id` INNER JOIN m_users muu ON muu.id=me.incharge LEFT JOIN `m_users` mu1 ON mu1.id = er.`user_1` LEFT JOIN m_users mu2 ON mu2.id = er.`user_2` LEFT JOIN m_users mu3 ON mu3.id = er.`user_3` INNER JOIN m_users muc ON muc.id=mc.incharge WHERE (er.user_1 =? OR er.user_2 =? OR er.user_3 =?)",
 		requestData.UserID, requestData.UserID, requestData.UserID)
-	
+
 	var events MyEvents
 	err := row.Scan(
 		&events.TeamName, &events.User1, &events.User1_Name, &events.User2, &events.User2_Name, &events.User3,
-		&events.User3_Name, &events.EIncharge,&events.CIncharge,&events.EventName, &events.Edesciption,&events.Category_id, &events.CategoryName,
-	&events.CDescription, &events.EventDate,
+		&events.User3_Name, &events.EIncharge, &events.CIncharge, &events.EventName, &events.Edesciption, &events.Category_id, &events.CategoryName,
+		&events.CDescription, &events.EventDate,
 	)
-	
-	
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -624,9 +622,6 @@ func GetRole(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
-
-
-
 
 type UsersRoleC struct {
 	ID       string `json:"id"`
