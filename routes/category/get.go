6bd611f78be83e6f29f175dd94ea645e2060bd01
POOVARASAN S <sivanperumal30772@gmail.com>
2023-11-02 12:02:27 +0530

    finally

--- routes/category/get.go
+++ routes/category/get.go
@@ -421,15 +421,17 @@ func GetMyEvents(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	row := config.Database.QueryRow(
-		"SELECT er.team_name, er.user_1, er.user_2, er.user_3, er.event_category_id, mu1.name AS user_1_name, mu2.name AS user_2_name, mu3.name AS user_3_name, muu.name AS event_incharge, me.event_name, me.description AS edescription, mc.category_name AS cname, mc.description AS cdescription,muc.name AS cincharge, me.event_date FROM `event_register` er INNER JOIN `event_categories` ec ON ec.`id` = er.`event_category_id` INNER JOIN m_category mc ON mc.id=er.event_category_id INNER JOIN m_events me ON me.id = ec.`event_id` INNER JOIN m_users muu ON muu.id=me.incharge INNER JOIN `m_users` mu1 ON mu1.id = er.`user_1` INNER JOIN m_users mu2 ON mu2.id = er.`user_2` INNER JOIN m_users mu3 ON mu3.id = er.`user_3` INNER JOIN m_users muc ON muc.id=mc.incharge WHERE (er.user_1 = ? OR er.user_2 =? OR er.user_3 = ?)",
+		"SELECT er.team_name, er.user_1, mu1.name AS user_1_name, er.user_2,mu2.name AS user_2_name, er.user_3,mu3.name AS user_3_name, muu.name AS event_incharge,muc.name AS cincharge, me.event_name, me.description AS edescription,er.event_category_id, mc.category_name AS cname, mc.description AS cdescription, me.event_date FROM `event_register` er INNER JOIN `event_categories` ec ON ec.`id` = er.`event_category_id` INNER JOIN m_category mc ON mc.id=er.event_category_id INNER JOIN m_events me ON me.id = ec.`event_id` INNER JOIN m_users muu ON muu.id=me.incharge INNER JOIN `m_users` mu1 ON mu1.id = er.`user_1` INNER JOIN m_users mu2 ON mu2.id = er.`user_2` INNER JOIN m_users mu3 ON mu3.id = er.`user_3` INNER JOIN m_users muc ON muc.id=mc.incharge WHERE (er.user_1 = ? OR er.user_2 =? OR er.user_3 = ?)",
 		requestData.UserID, requestData.UserID, requestData.UserID)
 	
 	var events MyEvents
 	err := row.Scan(
 		&events.TeamName, &events.User1, &events.User1_Name, &events.User2, &events.User2_Name, &events.User3,
-		&events.User3_Name, &events.EIncharge, &events.CIncharge, &events.EventName, &events.Edesciption, &events.EventDate,
-		&events.CategoryName, &events.CDescription, &events.Category_id,
+		&events.User3_Name, &events.EIncharge,&events.CIncharge,&events.EventName, &events.Edesciption,&events.Category_id, &events.CategoryName,
+	&events.CDescription, &events.EventDate,
 	)
+	
+	
 
 	if err != nil {
 		if err == sql.ErrNoRows {
