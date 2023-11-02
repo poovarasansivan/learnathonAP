9e7f74f5e6ddf12b586de76ebbdfb127ade831bb
POOVARASAN S <sivanperumal30772@gmail.com>
2023-11-02 11:09:22 +0530

    finaly

--- routes/category/get.go
+++ routes/category/get.go
@@ -422,8 +422,10 @@ func GetMyEvents(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-	row := config.Database.QueryRow("SELECT er.team_name, er.user_1, er.user_2, er.user_3, er.event_category_id, mu1.name AS user_1_name, mu2.name AS user_2_name, mu3.name AS user_3_name, muu.name AS event_incharge, me.event_name, me.description AS edescription, mc.category_name AS cname, mc.description AS cdescription, me.event_date FROM `event_register` er INNER JOIN `event_categories` ec ON ec.`id` = er.`event_category_id` INNER JOIN m_category mc ON mc.id=er.event_category_id INNER JOIN m_events me ON me.id = ec.`event_id` INNER JOIN m_users muu ON muu.id=me.incharge INNER JOIN `m_users` mu1 ON mu1.id = er.`user_1` INNER JOIN m_users mu2 ON mu2.id = er.`user_2` INNER JOIN m_users mu3 ON mu3.id = er.`user_3` WHERE (er.user_1 =? OR er.user_2 = ? OR er.user_3 = ?))", requestData.UserID, requestData.UserID, requestData.UserID)
-
+	row := config.Database.QueryRow(
+		"SELECT er.team_name, er.user_1, er.user_2, er.user_3, er.event_category_id, mu1.name AS user_1_name, mu2.name AS user_2_name, mu3.name AS user_3_name, muu.name AS event_incharge, me.event_name, me.description AS edescription, mc.category_name AS cname, mc.description AS cdescription, me.event_date FROM `event_register` er INNER JOIN `event_categories` ec ON ec.`id` = er.`event_category_id` INNER JOIN m_category mc ON mc.id=er.event_category_id INNER JOIN m_events me ON me.id = ec.`event_id` INNER JOIN m_users muu ON muu.id=me.incharge INNER JOIN `m_users` mu1 ON mu1.id = er.`user_1` INNER JOIN m_users mu2 ON mu2.id = er.`user_2` INNER JOIN m_users mu3 ON mu3.id = er.`user_3` WHERE (er.user_1 = ? OR er.user_2 = ? OR er.user_3 = ?)",
+		requestData.UserID, requestData.UserID, requestData.UserID)
+	
 	var events MyEvents
 	err := row.Scan(
 		&events.TeamName, &events.User1, &events.User1_Name, &events.User2, &events.User2_Name, &events.User3,
