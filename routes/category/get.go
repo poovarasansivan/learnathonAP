49d06116dc6e2bba267cea4c78146b7da977a813
POOVARASAN S <sivanperumal30772@gmail.com>
2023-11-02 12:06:21 +0530

    totally

--- routes/category/get.go
+++ routes/category/get.go
@@ -20,6 +20,16 @@ type Category struct {
 	Description  string `json:"description"`
 	InchargeName string `json:"incharge"`
 	MaxTeam      int    `json:"max_team"`
+	Registerstatus int `josn:""`
+}
+
+type CategoryC struct {
+	Id           int    `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	InchargeName string `json:"incharge"`
+	MaxTeam      int    `json:"max_team"`
+	Registerstatus int `json:"registration"`
 }
 
 type Input struct {
@@ -74,7 +84,7 @@ func GetAllCategory(w http.ResponseWriter, r *http.Request) {
 // to get particular category details
 func GetDetail(w http.ResponseWriter, r *http.Request) {
 	var response map[string]interface{}
-	var categories Category
+	var categories CategoryC
 	var input Input
 	err := json.NewDecoder(r.Body).Decode(&input)
 
@@ -87,7 +97,7 @@ func GetDetail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = config.Database.QueryRow("SELECT mc.id,mc.category_name,mc.description,mu.name,mc.max_team,mc.registration FROM event_categories ec INNER JOIN m_category mc ON mc.id = ec.category_id INNER JOIN m_events mee ON mee.id = ec.event_id INNER JOIN m_users mu ON mu.id = mc.incharge WHERE ec.status = '1' AND ec.category_id = ?", input.Id).Scan(&categories.Id, &categories.Name, &categories.Description, &categories.InchargeName, &categories.MaxTeam)
+	err = config.Database.QueryRow("SELECT mc.id,mc.category_name,mc.description,mu.name,mc.max_team,mc.registration FROM event_categories ec INNER JOIN m_category mc ON mc.id = ec.category_id INNER JOIN m_events mee ON mee.id = ec.event_id INNER JOIN m_users mu ON mu.id = mc.incharge WHERE ec.status = '1' AND ec.category_id = ?", input.Id).Scan(&categories.Id, &categories.Name, &categories.Description, &categories.InchargeName, &categories.MaxTeam,&categories.Registerstatus)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
