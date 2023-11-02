8fd5ee0c23d0a3ae6f62d4622260f7e5cfc61f70
POOVARASAN S <sivanperumal30772@gmail.com>
2023-11-02 14:27:27 +0530

    Final

--- routes/category/get.go
+++ routes/category/get.go
@@ -218,19 +218,19 @@ func GetTeams(w http.ResponseWriter, r *http.Request) {
 
 // to get team and team details
 type TeamI struct {
-	ID               int            `json:"id"`
-	TeamName         string         `json:"team_name"`
-	EventCategoryID  int            `json:"event_category_id"`
-	User1            string         `json:"user_1"`
-	User1name        string         `json:"name1"`
-	User2            sql.NullString `json:"user_2"`
-	User2name        string         `json:"name2"`
-	User3            sql.NullString `json:"user_3"`
-	User3name        string         `json:"name3"`
-	CreatedBy        string         `json:"created_by"`
-	CategoryName     string         `json:"category_name"`
-	TeamLeaderName   string         `json:"namet"`
-	TeamLeaderMobile int            `json:"phone"`
+	ID               int     `json:"id"`
+	TeamName         string  `json:"team_name"`
+	EventCategoryID  int     `json:"event_category_id"`
+	User1            string  `json:"user_1"`
+	User1name        string  `json:"name1"`
+	User2            *string `json:"user_2"`
+	User2name        *string `json:"name2"`
+	User3            *string `json:"user_3"`
+	User3name        *string `json:"name3"`
+	CreatedBy        string  `json:"created_by"`
+	CategoryName     string  `json:"category_name"`
+	TeamLeaderName   string  `json:"namet"`
+	TeamLeaderMobile int     `json:"phone"`
 }
 
 func GetTeamByID(w http.ResponseWriter, r *http.Request) {
