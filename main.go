30d22af56f64fd81773df38d65d9d2a89a5953f4
POOVARASAN S <86420216+poovarasansivan@users.noreply.github.com>
2023-11-27 12:16:45 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -4,10 +4,31 @@ import (
 	"fmt"
 	"learnathon/config"
 	"learnathon/routes"
+	"learnathon/routes/actionstatus"
+	"learnathon/routes/allcategory"
+	"learnathon/routes/answers"
+	"learnathon/routes/assignedquestions"
 	"learnathon/routes/auth"
-	"learnathon/routes/category"
+	"learnathon/routes/mcq"
+
+	"learnathon/routes/categorycount"
+	"learnathon/routes/categorydata"
+	"learnathon/routes/categorydetails"
+	"learnathon/routes/eventcategory"
+	"learnathon/routes/events"
+	"learnathon/routes/getmyevents"
 	"learnathon/routes/image"
+	"learnathon/routes/inserteventdata"
+	"learnathon/routes/overallusers"
+	"learnathon/routes/questions"
+	"learnathon/routes/registercount"
+	"learnathon/routes/registerdata"
+	"learnathon/routes/roles"
 	"learnathon/routes/rubrics"
+	"learnathon/routes/teamdetails"
+	"learnathon/routes/teams"
+	"learnathon/routes/topics"
+	"learnathon/routes/userdetails"
 
 	"net/http"
 	"os"
@@ -28,44 +49,46 @@ func main() {
 	router.HandleFunc("/api/", routes.Sample).Methods("POST")
 	router.HandleFunc("/api/auth/login", auth.Login).Methods("POST")
 
-	router.HandleFunc("/api/category/getAll", category.GetAllCategory).Methods("GET")
-	router.HandleFunc("/api/category/getDetails", category.GetDetail).Methods("POST")
-	router.HandleFunc("/api/users/{rollno}", category.GetUserByName).Methods("GET")
-	router.HandleFunc("/api/insertData", category.InsertData).Methods("POST")
-	router.HandleFunc("/api/users", category.GetUsers).Methods("GET")
-	router.HandleFunc("/api/teams", category.GetTeams).Methods("GET")
-	router.HandleFunc("/api/category/GetEvents", category.GetAllEvents).Methods("GET")
-	router.HandleFunc("/api/teamsid/{team_id}", category.GetTeamByID).Methods("GET")
-	router.HandleFunc("/api/CheckTeam", category.CheckTeam).Methods("POST")
-	router.HandleFunc("/api/GetMyEvents", category.GetMyEvents).Methods("POST")
-	router.HandleFunc("/api/GetEVCategory", category.GetAllEVCategory).Methods("GET")
-	router.HandleFunc("/api/GetCcount", category.GetCcount).Methods("GET")
-	router.HandleFunc("/api/GetRcount", category.GetRegisterCount).Methods("GET")
-	router.HandleFunc("/api/GetUserRole", category.GetRole).Methods("POST")
-	router.HandleFunc("/api/GetUserRoleC", category.GetRoleC).Methods("POST")
-	router.HandleFunc("/api/Insertcategory", category.InsertcategoryData).Methods("POST")
-	router.HandleFunc("/api/GetUserAdd", category.GetCRole).Methods("POST")
-	router.HandleFunc("/api/AddEvents", category.InsertEventData).Methods("POST")
-	router.HandleFunc("/api/GetCategoryC", category.GetCategoryCountR)
-	router.HandleFunc("/api/GetCName", category.GetCategoryName).Methods("GET")
-	router.HandleFunc("/api/GetTopics", category.GetTopics).Methods("POST")
-	router.HandleFunc("/api/insertQuestion", category.InsertQuestions).Methods("POST")
-	router.HandleFunc("/api/GetMyQuestion", category.GetMyQuestions).Methods("POST")
-	router.HandleFunc("/api/TotalQuestion", category.TotalQuestions).Methods("GET")
-	router.HandleFunc("/api/GetAllQuestion", category.GetAllQuestions).Methods("POST")
-	router.HandleFunc("/api/GetMyCategory", category.GetMyCategorys).Methods("POST")
-	router.HandleFunc("/api/updateAssigned", category.UpdateAssignedStatus).Methods("POST")
-	router.HandleFunc("/api/InsertAssignQuestion", category.InsertQuestionAssigned).Methods("POST")
-	router.HandleFunc("/api/GetMyassignQuestions", category.GetMyassign).Methods("POST")
-	// router.HandleFunc("/api/uploadImage", category.UploadImage).Methods("POST")
-	router.HandleFunc("/api/InsertAnswer", category.InsertAnswers).Methods("POST")
-	router.HandleFunc("/api/ButtonStatus", category.ButtonActionStatus).Methods("GET")
-	router.HandleFunc("/api/RubricsData", category.InsertRubricsData).Methods("POST")
-
+	router.HandleFunc("/api/getAll", eventcategory.GetAllCategory).Methods("GET")
+	router.HandleFunc("/api/getDetails", categorydetails.GetDetail).Methods("POST")
+	router.HandleFunc("/api/userdetails/{rollno}", userdetails.GetUserByName).Methods("GET")
+    router.HandleFunc("/api/insertData", registerdata.InsertData).Methods("POST")
+	router.HandleFunc("/api/users", overallusers.GetUsers).Methods("GET")
+	router.HandleFunc("/api/teams", teams.GetTeams).Methods("GET")
+	router.HandleFunc("/api/GetEvents", events.GetAllEvents).Methods("GET")
+	router.HandleFunc("/api/teamsid/{team_id}", teamdetails.GetTeamByID).Methods("GET")
+	router.HandleFunc("/api/CheckTeam", registerdata.CheckTeam).Methods("POST")
+	router.HandleFunc("/api/GetMyEvents", getmyevents.GetMyEvents).Methods("POST")
+	router.HandleFunc("/api/GetEVCategory", allcategory.GetAllEVCategory).Methods("GET")
+    router.HandleFunc("/api/GetCcount", categorycount.GetCcount).Methods("GET")
+	router.HandleFunc("/api/GetRcount", registercount.GetRegisterCount).Methods("GET")
+	router.HandleFunc("/api/GetUserRole", roles.GetRole).Methods("POST")
+	router.HandleFunc("/api/GetUserRoleC", roles.GetRoleC).Methods("POST")	
+	router.HandleFunc("/api/Insertcategory", categorydata.InsertcategoryData).Methods("POST")
+	router.HandleFunc("/api/GetUserAdd", roles.GetCRole).Methods("POST")
+	router.HandleFunc("/api/AddEvents", inserteventdata.InsertEventData).Methods("POST")
+	router.HandleFunc("/api/GetCategoryC", roles.GetCategoryCountR)
+    router.HandleFunc("/api/GetCName", categorydata.GetCategoryName).Methods("GET")
+    router.HandleFunc("/api/GetTopics", topics.GetTopics).Methods("POST")
+	router.HandleFunc("/api/insertQuestion", questions.InsertQuestions).Methods("POST")
+	router.HandleFunc("/api/GetMyQuestion", questions.GetMyQuestions).Methods("POST")
+	router.HandleFunc("/api/TotalQuestion", questions.TotalQuestions).Methods("GET")
+	router.HandleFunc("/api/GetAllQuestion", questions.GetAllQuestions).Methods("POST")
+	router.HandleFunc("/api/GetMyCategory", getmyevents.GetMyCategorys).Methods("POST")
+	router.HandleFunc("/api/updateAssigned", assignedquestions.UpdateAssignedStatus).Methods("POST")
+	router.HandleFunc("/api/InsertAssignQuestion", assignedquestions.InsertQuestionAssigned).Methods("POST")
+	router.HandleFunc("/api/GetMyassignQuestions", assignedquestions.GetMyassign).Methods("POST")
+    router.HandleFunc("/api/InsertAnswer", answers.InsertAnswers).Methods("POST")
+	router.HandleFunc("/api/ButtonStatus", actionstatus.ButtonActionStatus).Methods("GET")
+	router.HandleFunc("/api/RubricsData", rubrics.InsertRubricsData).Methods("POST")
+	router.HandleFunc("/api/QuestionStatus", actionstatus.Questionstatus).Methods("POST")
+	router.HandleFunc("/api/QuestionSubmit", actionstatus.GetQuestionSubmitstatus).Methods("POST")
+	router.HandleFunc("/api/McqEvalution", mcq.McqEvalution).Methods("GET")
+	router.HandleFunc("/api/MyMcq", mcq.Mymcqquestions).Methods("POST")
 	router.HandleFunc("/api/rubrics/getAll", rubrics.GetRubrics).Methods("GET")
-
 	router.HandleFunc("/api/uploadImage", image.Upload).Methods("POST")
 	router.HandleFunc("/api/serveImage/{filename}", image.ServeImage).Methods("GET")
+	router.HandleFunc("/api/McqQuestions", mcq.McqQuestions).Methods("POST")
 
 	c := cors.AllowAll()
 
